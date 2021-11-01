02373b1e090d6841250cabe603e2d3cbb5252979
Vitor Medeiros <vitordaniel602@gmail.com>
2021-11-01 16:14:29 -0300

    Código parcial das telas do módulo de Administrador

--- main.c
+++ main.c
@@ -16,6 +16,8 @@ void telaSobre(void);
 void telaPrincipal(void);
 void telaEquipe(void);
 void telaAdministrador(void);
+void telaAdministradorClientes(void);
+void telaAdministradorTransacoes(void);
 void telaCliente(void);
 void telaRelatorios(void);
 
@@ -24,13 +26,20 @@ void telaRelatorios(void);
 int main(void) {
     //Execução
     telaPrincipal();
+    telaSobre();
+    telaEquipe();
+    telaAdministrador();
+    telaAdministradorClientes();
+    telaAdministradorTransacoes();
+    telaCliente();
+    telaRelatorios();
     return 0;
 }
 
 /////
 // Funções
 void telaPrincipal(void) {
-    system("clear||cls");
+    //system("clear||cls");
     printf("\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("///                                                                         ///\n");
@@ -54,8 +63,10 @@ void telaPrincipal(void) {
     printf("///                                                                         ///\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
 
-    int escolha;
+    /*int escolha;
     printf("Digite o número referente ao módulo que deseja acessar: ");
     scanf("%d", &escolha);
 
@@ -64,11 +75,11 @@ void telaPrincipal(void) {
     else if(escolha==3) telaRelatorios(); 
     else if(escolha==4) telaSobre();
     else if(escolha==5) telaEquipe();  
-    else exit(0);
+    else exit(0);*/
 }
 
 void telaSobre(void) {
-    system("clear||cls");
+    //system("clear||cls");
     printf("\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("///                                                                         ///\n");
@@ -91,11 +102,13 @@ void telaSobre(void) {
     printf("///                                                                         ///\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
     
 }
 
 void telaEquipe(void) {
-    system("clear||cls");
+    //system("clear||cls");
     printf("\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("///                                                                         ///\n");
@@ -119,10 +132,13 @@ void telaEquipe(void) {
     printf("///                                                                         ///\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
 }
 
+//telas do módulo 1-Administrador
 void telaAdministrador(void) {
-    system("clear||cls");
+    //system("clear||cls");
     printf("\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("///                                                                         ///\n");
@@ -137,16 +153,143 @@ void telaAdministrador(void) {
     printf("///                                                                         ///\n");
     printf("///       = = = = = Sistema de Controle de Contas Bancárias = = = = =       ///\n");
     printf("///                                                                         ///\n");
-    printf("///            1. Clientes                                                  ///\n");
-    printf("///            2. Transações                                                ///\n");
-    printf("///            0. Sair                                                      ///\n");
+    printf("///            1. Lista de Clientes                                         ///\n");
+    printf("///            2. Cadastrar Cliente                                         ///\n");
+    printf("///            3. Pesquisar Dados de um Cliente                             ///\n");
+    printf("///            4. Atualizar Cliente                                         ///\n");
+    printf("///            5. Transações                                                ///\n");
+    printf("///            0. Voltar ao menu anterior                                   ///\n");
+    printf("///                                                                         ///\n");
+    printf("///           Escolha a opção desejada:                                     ///\n");
     printf("///                                                                         ///\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
 }
 
+void telaAdministradorClientes(void) {
+    //system("clear||cls");
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
+    printf("///                 Centro de Ensino Superior do Seridó                     ///\n");
+    printf("///               Departamento de Computação e Tecnologia                   ///\n");
+    printf("///                  Disciplina DCT1106 -- Programação                      ///\n");
+    printf("///               Projeto Sistema de Controle de Contas Bancárias           ///\n");
+    printf("///     Copyright © 2021 Vitor Daniel - Todos os direitos reservados        ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///       = = = = = Sistema de Controle de Contas Bancárias = = = = =       ///\n");
+    printf("///                                                                         ///\n");
+    printf("///                        LISTA DE CLIENTES (N0ME E CPF)                   ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            1. Vitor Daniel Lócio Medeiros (101.101.101-11)              ///\n");
+    printf("///            2. Maria José Lócio Linhares Medeiros (101.101.101-11)       ///\n");
+    printf("///            3. Jesca Gabrielly Medeiros (101.101.101-11)                 ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
+}
+
+void telaAdministradorCadastroCliente(void) {
+    //system("clear||cls");
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
+    printf("///                 Centro de Ensino Superior do Seridó                     ///\n");
+    printf("///               Departamento de Computação e Tecnologia                   ///\n");
+    printf("///                  Disciplina DCT1106 -- Programação                      ///\n");
+    printf("///               Projeto Sistema de Controle de Contas Bancárias           ///\n");
+    printf("///     Copyright © 2021 Vitor Daniel - Todos os direitos reservados        ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///       = = = = = Sistema de Controle de Contas Bancárias = = = = =       ///\n");
+    printf("///                                                                         ///\n");
+    printf("///       = = = = = = = = = = Cadastro de Cliente = = = = = = = = = =       ///\n");
+    printf("///           Nome completo:                                                ///\n");
+    printf("///           CPF (apenas números):                                         ///\n");
+    printf("///           E-mail:                                                       ///\n");
+    printf("///           Data de Nascimento (dd/mm/aaaa):                              ///\n");
+    printf("///           Celular  (apenas números):                                    ///\n");
+    printf("///           Cidade:                                                       ///\n");
+    printf("///           Logradouro / Número:                                          ///\n");
+    printf("///           Bairro:                                                       ///\n");
+    printf("///           CEP:                                                          ///\n");
+    printf("///           Complemento:                                                  ///\n");
+    printf("///                                                                         ///\n");
+    printf("///           Escolha a opção desejada:                                     ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
+}
+
+void telaAdministradorPesquisarCliente(void) {
+    //system("clear||cls");
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
+    printf("///                 Centro de Ensino Superior do Seridó                     ///\n");
+    printf("///               Departamento de Computação e Tecnologia                   ///\n");
+    printf("///                  Disciplina DCT1106 -- Programação                      ///\n");
+    printf("///               Projeto Sistema de Controle de Contas Bancárias           ///\n");
+    printf("///     Copyright © 2021 Vitor Daniel - Todos os direitos reservados        ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///       = = = = = Sistema de Controle de Contas Bancárias = = = = =       ///\n");
+    printf("///                                                                         ///\n");
+    printf("///       = = = = = = = = = = Pesquisa de Cliente = = = = = = = = = =       ///\n");
+    printf("///                                                                         ///\n");
+    printf("///           Informe o CPF (apenas números) do cliente:                    ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
+}
+
+void telaAdministradorTransacoes(void) {
+    //system("clear||cls");
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
+    printf("///                 Centro de Ensino Superior do Seridó                     ///\n");
+    printf("///               Departamento de Computação e Tecnologia                   ///\n");
+    printf("///                  Disciplina DCT1106 -- Programação                      ///\n");
+    printf("///               Projeto Sistema de Controle de Contas Bancárias           ///\n");
+    printf("///     Copyright © 2021 Vitor Daniel - Todos os direitos reservados        ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///       = = = = = Sistema de Controle de Contas Bancárias = = = = =       ///\n");
+    printf("///                                                                         ///\n");
+    printf("///                            LISTA DE TRANSAÇÕES                          ///\n");
+    printf("///            1. Saque - Vitor Daniel Lócio Medeiros - R$1000,00           ///\n");
+    printf("///            2. Depósito - Maria José Lócio Linhares Medeiros - R$500,00  ///\n");
+    printf("///            3. Solicitação de Empréstimo - Jesca Gabrielly Medeiros      ///\n");
+    printf("///            - R$10000                                                    ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
+}
+
+//fim das telas do módulo 1-Administrador
+
 void telaCliente(void) {
-    system("clear||cls");
+    //system("clear||cls");
     printf("\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("///                                                                         ///\n");
@@ -170,10 +313,12 @@ void telaCliente(void) {
     printf("///                                                                         ///\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
 }
 
 void telaRelatorios(void) {
-    system("clear||cls");
+    //system("clear||cls");
     printf("\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("///                                                                         ///\n");
@@ -193,4 +338,6 @@ void telaRelatorios(void) {
     printf("///                                                                         ///\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
 }
