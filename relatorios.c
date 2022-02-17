fbd7452b88b9c50771518732c206479dd9eb69cc
Vitor Daniel Lócio Medeiros <vitordaniel602@gmail.com>
2022-02-17 13:11:15 -0300

    Tentavida do relatório dinâmico

--- relatorios.c
+++ relatorios.c
@@ -10,17 +10,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include "relatorios.h"
 #include "validators.h"
 
+typedef struct cli Cli; //STRUCT INSPIRADO POR @flgorgoni
 
 ////// Funções do Módulo de Relatórios
 void moduloRelatorios(void) {
     char opcao;
+    Cli *lista;
+    lista = NULL;
     do {
         opcao = telaRelatorios();
         switch(opcao) {
-            case '1':   telaRelatorios();
+            case '1':   clientesOrdemAlfabetica(&lista);
+                        telaClientesOrdemAlfabetica(lista);
+                        free(lista);
                         break;
         }
     } while (opcao != '0');
@@ -43,13 +49,83 @@ char telaRelatorios(void) {
     printf("///                                                                         ///\n");
     printf("///       = = = = = Sistema de Controle de Contas Bancárias = = = = =       ///\n");
     printf("///                                                                         ///\n");
-    printf("///            1. Logs do Sistema                                           ///\n");
+    printf("///            1. Lista de Clientes por ordem alfabética                    ///\n");
+    printf("///            2. Lista de Clientes por ordem alfabética decrescente        ///\n");
     printf("///            0. Voltar ao menu anterior                                   ///\n");
     printf("///                                                                         ///\n");
     printf("///            Escolha a opção desejada: ");                                
     scanf("%c", &escolha);
     getchar();
+    printf("///                                                                         ///\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("\n");
     return escolha;
 }
+
+void apagarLista(Cli **lista)
+{
+    Cli *cl;
+    
+    while (*lista != NULL){
+        cl = *lista;
+        *lista = (*lista)->prox;
+        free(cl);
+    }
+    //printf("Lista excluida com sucesso! \n");    
+}
+
+void clientesOrdemAlfabetica(Cli **lista)
+{
+    FILE *fp;
+    Cli *cl;
+    
+    apagarLista(&(*lista));
+    *lista = NULL;
+    fp = fopen("clientes.dat","rb");
+    if (fp == NULL)
+    {
+        printf("O programa não conseguiu abrir o arquivo! \n");
+        exit(1);
+    }
+    else{
+        cl = (Cli *) malloc(sizeof(Cli));
+        while (fread(cl, sizeof(Cli), 1, fp)){
+            if ((*lista == NULL) || (strcmp(cl->nome, (*lista)->nome) < 0)) {
+                cl->prox = *lista;
+                *lista = cl;
+            }else{
+                Cli* ant = *lista;    
+                Cli* atu = (*lista)->prox;
+                while ((atu != NULL) && (strcmp(atu->nome, cl->nome) < 0)) {
+                    ant = atu;
+                    atu = atu->prox;
+                }
+                ant->prox = cl;
+                cl->prox = atu;
+            }
+            cl = (Cli *) malloc(sizeof(Cli));
+        }
+        free(cl);
+        printf("Arquivo recuperado com sucesso! \n");
+    }   
+    fclose(fp); 
+}
+
+void telaClientesOrdemAlfabetica(Cli *aux)
+{
+  printf("\n\n");
+  printf("****************************************\n");
+    printf("*** Relatorio dos Clientes Cadastrados ***\n");
+  printf("****************************************\n");
+    printf("Cliente\n");
+  printf("\n");
+    while (aux != NULL)
+    {
+        printf("%s\n",aux->nome);
+        aux = aux->prox;
+    }
+    printf("\nFim da Lista! \n\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
+}
+
